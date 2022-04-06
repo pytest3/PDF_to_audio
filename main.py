928c905384b37958f04f9bc8b21ca79a66e36185
Zac <prospectheory@gmail.com>
2022-04-06 16:47:18 +0800

    Append all the individual mp3 files to a list

--- main.py
+++ main.py
@@ -25,7 +25,6 @@ text_limit = 10000
 split_text = []
 for i in range(0, len(full_text), text_limit):
     split_text.append(full_text[i:i+text_limit])
-    print(len(full_text[i:i+text_limit]))
 
 # Create a boto3 session
 polly_client = boto3.Session(aws_access_key_id=AWS_ACCESS_KEY_ID,
@@ -33,37 +32,41 @@ polly_client = boto3.Session(aws_access_key_id=AWS_ACCESS_KEY_ID,
                              region_name=region).client('polly')
 
 
+audio_files = []
+for count, text in enumerate(split_text):
+    # Request speech synthesis
+    response = polly_client.start_speech_synthesis_task(VoiceId='Joanna',
+                                                        OutputS3BucketName=AWS_BUCKET_NAME,
+                                                        OutputFormat='mp3',
+                                                        Text=str(text),
+                                                        SnsTopicArn=TOPIC_ARN,
+                                                        Engine='neural')
+    # Get mp3 file uri from response
+    mp3_uri = response['SynthesisTask']['OutputUri']
 
-# Request speech synthesis
-response = polly_client.start_speech_synthesis_task(VoiceId='Joanna',
-                                                    OutputS3BucketName=AWS_BUCKET_NAME,
-                                                    OutputFormat='mp3',
-                                                    Text=str(full_text),
-                                                    SnsTopicArn=TOPIC_ARN,
-                                                    Engine='neural')
+    # Wait for speech to be fully synthesised
+    retries = 0
+    max_retries = 20
+    still_try = True
+    while retries < max_retries and still_try == True:
+        if requests.get(mp3_uri, headers=headers).status_code != 200:
+            retries += 1
+            print(f'mp3 file not ready..')
+            sleep(60)
+        else:
+            print(f'mp3 file is ready..')
+            # Get synthesised speech as a mp3 file
+            response = requests.get(mp3_uri, headers=headers)
+            with open(f'audiobook_part_{count}.mp3', 'wb') as file:
+                audio_file = file.write(response.content)
+                audio_files.append(audio_file)
+            still_try = False
 
-# Get mp3 file uri from response
-mp3_uri = response['SynthesisTask']['OutputUri']
-
-# Wait for speech to be fully synthesised
-retries = 0
-max_retries = 30
-still_try = True
-while retries < max_retries and still_try == True:
-    if requests.get(mp3_uri, headers=headers).status_code != 200:
-        retries += 1
-        print(f'mp3 file not ready..')
-        sleep(60)
-    else:
-        print(f'mp3 file is ready..')
-        audiobook = requests.get(mp3_uri, headers=headers)
-        with open('audiobook.mp3', 'wb') as file:
-            file.write(audiobook.content)
-        still_try = False
+print(len(audio_files))
 
 # Play audiobook
-if sys.platform == "win32":
-    os.startfile('audiobook.mp3')
-else:
-    opener = "open" if sys.platform == "darwin" else "xdg-open"
-    subprocess.call([opener, 'audiobook.mp3'])
+# if sys.platform == "win32":
+#     os.startfile('audiobook.mp3')
+# else:
+#     opener = "open" if sys.platform == "darwin" else "xdg-open"
+#     subprocess.call([opener, 'audiobook.mp3'])
